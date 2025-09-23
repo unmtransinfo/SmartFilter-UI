737c476c3d9a2ad9c8a51b7d25e94791c80d102f
lifwolf <lifwolf@Bivek.myguest.virtualbox.org>
2025-09-23 13:47:15 -0600

    some cleaning.

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import SmartFilterLayout from "./components/SmartFilterLayout";
 import SmartsFilterResult from "./components/SmartsFilterResult";
 
